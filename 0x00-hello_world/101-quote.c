8c9d3f188d165a9d01eec7b24b2f2e2a7b64c568
ZEFZAFnouhayla <nohaila.zefzaf@gmail.com>
2023-03-16 11:10:06 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints exactly and that piece of art is useful
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
