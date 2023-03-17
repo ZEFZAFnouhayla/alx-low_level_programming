8827e7b5218729606584a2e919226877e0fdf5da
ZEFZAFnouhayla <nohaila.zefzaf@gmail.com>
2023-03-17 09:40:26 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - A program that print the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	putchar(a);
+	putchar('\n');
+
+	return (0);
+}
